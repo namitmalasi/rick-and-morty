9dddc2110323cdf5b3a2a853a4caa5b6759aae32
Namit Malasi <namitmalasi@gmail.com>
2024-03-21 19:42:37 +0530

    implmented the search functionality

--- src/Home.jsx
+++ src/Home.jsx
@@ -4,15 +4,40 @@ import Footer from "./components/Footer";
 import Input from "./components/Input";
 import Button from "./components/Button";
 import CardsList from "./components/CardsList";
+import Error from "./components/Error";
 
 const Home = () => {
+  const [initData, setInitData] = useState();
   const [characters, setCharacters] = useState();
+  const [searchTermCharacters, setSearchTermCharacters] = useState();
+  const [noResult, setNoResult] = useState(false);
+  const [totalCharacters, setTotalCharacters] = useState();
 
+  const searchTerm = (e) => {
+    setSearchTermCharacters(e.target.value);
+    searchCharacters();
+  };
+
+  const searchCharacters = () => {
+    if (searchTermCharacters) {
+      const searchResult = characters.filter((character) =>
+        character.name
+          .toLowerCase()
+          .includes(searchTermCharacters.toLowerCase())
+      );
+      setCharacters(searchResult);
+      setNoResult(searchResult.length === 0);
+    } else {
+      setCharacters(initData);
+    }
+  };
   const getCharacters = async () => {
     try {
       const data = await fetch("https://rickandmortyapi.com/api/character");
       const response = await data.json();
       setCharacters(response.results);
+      setInitData(response.results);
+      setTotalCharacters(response.info.count);
     } catch (error) {
       console.log(error);
     }
@@ -27,9 +52,15 @@ const Home = () => {
       <Input
         type="search"
         placeholder="type your character..."
-        changeHandler={() => {}}
+        changeHandler={searchTerm}
       />
-      <CardsList characters={characters} />
+      {totalCharacters && (
+        <p className="mt-4">
+          Total Characters are:{" "}
+          <span className="font-bold">{totalCharacters}</span>
+        </p>
+      )}
+      {noResult ? <Error /> : <CardsList characters={characters} />}{" "}
       <Button clickHandler={() => {}}>Next</Button>
       <Footer />
     </section>
